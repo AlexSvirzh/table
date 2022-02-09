39f42def7587e60adf57cf5eac3bf4c5efc8f7cf
Alex Svirzh <90563111+AlexSvirzh@users.noreply.github.com>
2022-02-09 13:41:59 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-document.write("<tr><td></td>"); 
+document.write("<tr><td></td>") 
   for (i = 1; i <=10; i++) 
-    document.write("<td bgcolor='gray'>"+i+" </td>"); 
+    document.write("<td bgcolor='gray'>"+i+" </td>") 
 
-    document.write("</tr>"); 
-for (i = 1; i <=10; i++) 
-{ 
-document.write("<td bgcolor='gray'>" + i + " </td>"); 
-for (j = 1; j <= 10; j++) 
-{ 
-document.write("<td>" + (i*j) + " </td>");} 
-document.write("</tr>"); 
+    document.write("</tr>") 
+  for (i = 1; i <=10; i++) { 
+    document.write("<td bgcolor='gray'>" + i + " </td>") 
+  for (j = 1; j <= 10; j++) { 
+  document.write("<td>" + (i*j) + " </td>")
+  } 
+  document.write("</tr>") 
 } 
